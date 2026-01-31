f630a51c7426eca8448e5142ae38e9877f63a318
Matheus Kunzler Maldaner <matheus@sisnema.com.br>
2026-01-31 18:13:37 -0500

    astro deps

--- globe-lite/astro.config.mjs
+++ globe-lite/astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
+import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  adapter: node({ mode: 'standalone' }),
   integrations: [svelte()],
 
   vite: {
